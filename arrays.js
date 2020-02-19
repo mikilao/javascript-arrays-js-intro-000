d3cc8349670f34e7b62ac1c58fed4cbb5c839eed
Rachel S <queenuvchaos@gmail.com>
2020-02-19 17:56:32 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,15 @@
+var chocolateBars= [
+  "snickers",
+  "hundred grand",
+  "kitkat",
+  "skittles"
+]
+addElementToBeginningOfArray(array, element){
+      expect(array).to.eql(array)
+      expect(array).to.eql([1])
+}
+addElementToBeginningOfArray(array, 'foo'){
+
+      expect(array).to.eql(array)
+      expect(array).to.eql([1])
+    })
